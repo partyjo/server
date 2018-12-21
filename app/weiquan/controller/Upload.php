c40dad23cdc3df2f89abda37d206822294163962
Min Zhang <zhangmin@MindeMacBook-Pro.local>
2018-12-21 10:01:01 +0800

    add upload/base64

--- app/weiquan/controller/Upload.php
+++ app/weiquan/controller/Upload.php
@@ -37,4 +37,40 @@ class Upload extends Base
 
     return $this->ajax($this->data);
   }
+
+  public function base64()
+  {   
+    // $file = request()->file('postFile');
+    $image = input('post.postFile');
+    $imageName = "25220_".date("His",time())."_".rand(1111,9999).'.png';
+    if (strstr($image,",")){
+      $image = explode(',',$image);
+      $image = $image[1];
+    }
+    $path = $this->request['host'].'/public/upload/'.date("Ymd",time());
+    if (!is_dir($path)){ //判断目录是否存在 不存在就创建
+      mkdir($path,0777,true);
+    }
+    $imageSrc= $path."/". $imageName; //图片名字
+    $r = file_put_contents($imageSrc, base64_decode($image));//返回的是字节数
+    if ($r){
+      // 数据处理
+      $data = [];
+      $data['id'] = $this->createGuid();
+      $data['file_name'] = $imageName;
+      $data['file_path'] = $imageSrc;
+      $data['file_size'] = $r;
+      $data['file_type'] = 'png';
+      $data['create_time'] = $this->now();
+      $data['update_time'] = $this->now();
+
+      $this->data['data'] = $data;
+    } else {
+      // 上传失败获取错误信息
+      $this->data['code'] = 1001;
+      $this->data['msg'] = '上传失败';
+    }
+
+    return $this->ajax($this->data);
+  }
 }
